fb3d2e68ce2cb7444902467f99ce1998d3150d84
Vincent <eric61011@gmail.com>
2021-09-22 00:01:58 +0800

    Implement dialog of bank and currency type.

--- app/src/main/java/com/vincent/forexledger/activity/EditBookActivity.kt
+++ app/src/main/java/com/vincent/forexledger/activity/EditBookActivity.kt
@@ -5,15 +5,39 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.vincent.forexledger.R
-import kotlinx.android.synthetic.main.content_toolbar.*
+import com.vincent.forexledger.model.bank.BankType
+import com.vincent.forexledger.model.exchangerate.CurrencyType
+import kotlinx.android.synthetic.main.content_toolbar.toolbar
+import kotlinx.android.synthetic.main.fragment_edit_book.*
 
 class EditBookActivity : AppCompatActivity() {
-    
+
+    private var bankSelectingDialog: AlertDialog? = null
+    private var currencySelectingDialog: AlertDialog? = null
+
+    private var selectedBank: BankType? = null
+    private var selectedCurrencyType: CurrencyType? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_edit_book)
         initToolbar()
+
+        editBank.setOnClickListener {
+            if (bankSelectingDialog == null) {
+                initBankSelectingDialog()
+            }
+            bankSelectingDialog!!.show()
+        }
+
+        editCurrencyType.setOnClickListener {
+            if (currencySelectingDialog == null) {
+                initCurrencyDialog()
+            }
+            currencySelectingDialog!!.show()
+        }
     }
 
     private fun initToolbar() {
@@ -23,6 +47,33 @@ class EditBookActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    private fun initBankSelectingDialog() {
+        val bankTypes = BankType.values()
+        val bankLocalNames = bankTypes.map { getString(it.localNameResource) }.toTypedArray()
+        val builder = AlertDialog.Builder(this).setItems(bankLocalNames) { dialog, position ->
+            selectedBank = bankTypes[position]
+            editBank.setText(bankLocalNames[position])
+        }
+        bankSelectingDialog = builder.create()
+    }
+
+    private fun initCurrencyDialog() {
+        val currencyTypes = CurrencyType.values()
+        val currencyLocalNames = currencyTypes.map {
+            "${getString(it.localNameResource)} ${it.name}"
+        }.toTypedArray()
+
+        val builder = AlertDialog.Builder(this).setItems(currencyLocalNames) { dialog, position ->
+            selectedCurrencyType = currencyTypes[position]
+            editCurrencyType.setText(currencyLocalNames[position])
+        }
+        currencySelectingDialog = builder.create()
+    }
+
+    private fun createBook() {
+        Toast.makeText(this, "SUBMIT", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.edit_page_actions, menu)
         return super.onCreateOptionsMenu(menu)
@@ -30,7 +81,7 @@ class EditBookActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.action_submit) {
-            Toast.makeText(this, "SUBMIT", Toast.LENGTH_SHORT).show()
+            createBook()
         }
         return super.onOptionsItemSelected(item)
     }
