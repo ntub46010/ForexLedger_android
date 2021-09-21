c3ded054847edcf740b73d662a1f1a868a074b77
Vincent <eric61011@gmail.com>
2021-09-21 23:23:26 +0800

    Add submit button on toolbar.

--- app/src/main/java/com/vincent/forexledger/activity/EditBookActivity.kt
+++ app/src/main/java/com/vincent/forexledger/activity/EditBookActivity.kt
@@ -2,10 +2,14 @@ package com.vincent.forexledger.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.vincent.forexledger.R
 import kotlinx.android.synthetic.main.content_toolbar.*
 
 class EditBookActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_edit_book)
@@ -17,5 +21,17 @@ class EditBookActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbar.setNavigationOnClickListener { finish() }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.edit_page_actions, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_submit) {
+            Toast.makeText(this, "SUBMIT", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
