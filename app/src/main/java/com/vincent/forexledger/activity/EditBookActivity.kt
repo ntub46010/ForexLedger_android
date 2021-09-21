3410b4762a0e73f4b37554be64fa5c8912f20256
Vincent <eric61011@gmail.com>
2021-09-21 16:25:36 +0800

    Implement toolbar behavior.

--- app/src/main/java/com/vincent/forexledger/activity/EditBookActivity.kt
+++ app/src/main/java/com/vincent/forexledger/activity/EditBookActivity.kt
@@ -3,10 +3,19 @@ package com.vincent.forexledger.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vincent.forexledger.R
+import kotlinx.android.synthetic.main.content_toolbar.*
 
 class EditBookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_edit_book)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        toolbar.title = getString(R.string.create_book)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener { finish() }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
