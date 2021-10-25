95b09fad2b2671496921facc16d4f180859dabb3
Vincent <eric61011@gmail.com>
2021-10-26 00:25:37 +0800

    Implement transaction type dialog.

--- app/src/main/java/com/vincent/forexledger/fragment/CreateEntryFragment.kt
+++ app/src/main/java/com/vincent/forexledger/fragment/CreateEntryFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.vincent.forexledger.R
+import com.vincent.forexledger.model.entry.TransactionType
 
 class CreateEntryFragment : Fragment() {
+    private var entryTypeSelectingDialog: AlertDialog? = null
+
+    private var selectedEntryType: TransactionType? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_create_entry, container, false)
@@ -15,5 +20,15 @@ class CreateEntryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun initEntryTypeSelectingDialog() {
+        val entryTypes = TransactionType.values();
+        val entryTypeLocalNames = entryTypes.map { requireContext().getString(it.localNameResource) }.toTypedArray()
+        val builder = AlertDialog.Builder(requireContext())
+            .setItems(entryTypeLocalNames) { dialog, position ->
+                selectedEntryType = entryTypes[position]
+            }
+        entryTypeSelectingDialog = builder.create();
     }
 }
---
+++ app/src/main/java/com/vincent/forexledger/model/entry/TransactionType.kt
@@ -0,0 +1,11 @@
+package com.vincent.forexledger.model.entry
+
+import com.vincent.forexledger.R
+
+enum class TransactionType(val localNameResource: Int) {
+    TRANSFER_IN_FROM_TWD(R.string.transaction_type_transfer_in_from_twd),
+    TRANSFER_IN_FROM_FOREIGN(R.string.transaction_type_transfer_in_from_foreign),
+    TRANSFER_IN_FROM_INTEREST(R.string.transaction_type_transfer_in_from_interest),
+    TRANSFER_OUT_TO_TWD(R.string.transaction_type_transfer_out_to_twd),
+    TRANSFER_OUT_TO_FOREIGN(R.string.transaction_type_transfer_out_to_foreign)
+}
